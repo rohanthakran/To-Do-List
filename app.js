f2a119244f5a185cbf10b0e5fd48cf2ac40123e1
Rohan <31851961+rohanthakran@users.noreply.github.com>
2020-04-04 19:55:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,90 @@
+var DataContoller =(function(){
+
+    var data1 = function(description){
+        
+        this.description =description;
+    };
+    var data =[];
+
+    return {
+        additem: function(des){ 
+            var newItem;
+            
+            newItem  = new data1(des);
+            data.push(newItem);
+            return newItem;
+
+            
+        },
+       testing: function(){
+           console.log(data);
+       }
+    }
+})();
+
+
 var UiController = (function(){
+
+
         return{
             getInput : function(){
-                return{
+            return{
                  description : document.querySelector('.description').value
         
                 };
-              }
-        };
+            },
+                
+         addList :function(description){
+                    var html, newItem;
+                   html = '<div class="item"> <div class="check"><i class="far fa-check-circle"></i></div><p>%Description%</p><button class="delete"><i class="far fa-trash-alt"></i></button> </div> '
+                
+                  newItem = html.replace('%Description%', description);
+                  document.querySelector('.list1').insertAdjacentHTML('beforeend', newItem);
+                }
+            
+        }
+                 
+        
 })();
 
 
 
 
-var controller = (function(UICntr){
+var controller = (function(UICntr, DataC){
 
-        
+        var setupEventListners = function(){
+            document.querySelector('.btn').addEventListener('click', ctrlAddItem);
+            document.addEventListener('keypress', function(event){
+      
+                if(event.keyCode === 13 || event.which === 13){
+                    ctrlAddItem();
+                    
+                }
+              
+        });
+
+    };
         var ctrlAddItem = function(){
         // 1 . get the user filled input
         console.log("Enter was pressed");
         var input = UICntr.getInput();
-        console.log(input); 
+        
+        var newItem = DataC.additem(input.description);
+        console.log(newItem.description);
 
+        UICntr.addList(input.description);
 
         // 2. add the item to the budget contoller
 
         // 3. Add the item to the ui
         }
-       document.querySelector('.btn').addEventListener('click',ctrlAddItem);
-        document.addEventListener('keypress', function(event){
-  
-            if(event.keyCode === 13 || event.which === 13){
-                ctrlAddItem();
-                console.log("erere");
+      
+        return {
+            init: function(){
+                console.log("application is started");
+                setupEventListners();
+                
             }
-          
-    });
-})(UiController);
+        }
+})(UiController, DataContoller);
+controller.init();
